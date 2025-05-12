5c767a84a5c547d93c89675bb9b492e2f57fd5b3
hhw <1171604288@qq.com>
2025-05-12 13:54:06 +0800

    cryptoJS使用内部的

--- inject/tool.js
+++ inject/tool.js
@@ -1,28 +1,39 @@
-onLoad('https://cdn.bootcdn.net/ajax/libs/crypto-js/4.1.1/crypto-js.js');
 const NEXT_PUBLIC_TRIEVE_API_KEY = 'tr-Ef0O1GG473PDFHfclCabtti5n0mHNolw';
-function onLoad(url, times = 2) {
-  const script = document.createElement('script');
 
-  script.src = url;
-  script.onload = () => {
-    document.head.removeChild(script);
-  };
-
-  script.onerror = () => {
-    document.head.removeChild(script);
-    if (times > 0) {
-      onLoad(times - 1);
+function storeEncrypted(key, value) {
+  if (window.injectSrcipt) {
+    const cryptoJS = window.injectSrcipt(36023);
+    if (cryptoJS) {
+      const aes = window.injectSrcipt.n(cryptoJS)();
+      const stringifiedValue = JSON.stringify(value);
+      const encryptedValue = aes.AES.encrypt(stringifiedValue, key).toString();
+      localStorage.setItem('mintlify-auth', encryptedValue);
     }
-  };
-
-  document.head.appendChild(script);
+  }
 }
 
+function loadEncrypted(key) {
+  if (window.injectSrcipt) {
+    const cryptoJS = window.injectSrcipt(36023);
+    if (cryptoJS) {
+      const encryptedString = localStorage.getItem('mintlify-auth');
+      if (encryptedString != null) {
+        try {
+          const decryptedString = aes.AES.decrypt(encryptedString, key).toString(aes.enc.Utf8);
+          const decryptedValue = JSON.parse(decryptedString);
+          return decryptedValue;
+        } catch (e) {
+          console.log(`unable to decrypt stored credentials: ${e}`);
+        }
+      }
+    }
+  }
+}
 //window.postMessage({ dentity: 'ls', Authorization: '123' })
 window.addEventListener('message', (e) => {
   const data = e?.data;
   if (data?.dentity === 'ls') {
-    if (data.Authorization && window.CryptoJS?.AES) {
+    if (data.Authorization) {
       const Authorization = data.Authorization;
       const match = document.cookie.match(/(?:^|;\s*)mintlify-auth-key=([^;]*)/);
       if (match?.[1]) {
@@ -38,28 +49,7 @@ window.addEventListener('message', (e) => {
   }
 })
 
-function storeEncrypted(key, value) {
-  const stringifiedValue = JSON.stringify(value);
-  const encryptedValue = window.CryptoJS.AES.encrypt(stringifiedValue, key).toString();
-  localStorage.setItem('mintlify-auth', encryptedValue);
-}
-
-function loadEncrypted(key) {
-  const encryptedString = localStorage.getItem('mintlify-auth');
-  if (encryptedString != null) {
-    try {
-      const decryptedString = window.CryptoJS.AES.decrypt(encryptedString, key).toString(window.CryptoJS.enc.Utf8);
-      const decryptedValue = JSON.parse(decryptedString);
-      return decryptedValue;
-    } catch (e) {
-      console.log(`unable to decrypt stored credentials: ${e}`);
-    }
-  }
-}
-
-
 if (window.injectSrcipt) {
-
   // if (window.__NEXT_DATA__) {
   //   window.__NEXT_DATA__.props.pageProps.subdomain = "docs.hhw31.com";
   //   window.__NEXT_DATA__.props.pageProps.actualSubdomain = "hc-2ade1025";
